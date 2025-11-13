86de95aaea259748cee6cd22decdcaa693504a44
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-11-13 11:26:46 -0300

    refactor: se refactorizaron los estilos de los formularios de autenticacion

--- src/components/auth/PersonalInforForm.jsx
+++ src/components/auth/PersonalInforForm.jsx
@@ -36,71 +36,60 @@ import { Form, Row, Col } from 'react-bootstrap';
  */
 const PersonalInfoForm = ({ formData, handleChange }) => (
   <>
-    <Form.Group as={Row} className="mb-2">
-      <Form.Label column sm={4} className="form-label text-sm-end">Email</Form.Label>
-      <Col sm={8}>
-        <Form.Control
-          required
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="form-control input"
-        />
-      </Col>
+    <Form.Group className="mb-2">
+      <Form.Control
+        required
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        className="form-control input wide-input"
+        placeholder="Correo electrónico"
+      />
     </Form.Group>
-    <Form.Group as={Row} className="mb-2">
-      <Form.Label column sm={4} className="form-label text-sm-end">Nombre</Form.Label>
-      <Col sm={8}>
-        <Form.Control
-          required
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="form-control input"
-        />
-      </Col>
+    <Form.Group className="mb-2">
+      <Form.Control
+        required
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        className="form-control input wide-input"
+        placeholder="Nombre"
+      />
     </Form.Group>
-    <Form.Group as={Row} className="mb-2">
-      <Form.Label column sm={4} className="form-label text-sm-end">Apellido</Form.Label>
-      <Col sm={8}>
-        <Form.Control
-          required
-          type="text"
-          name="lastname"
-          value={formData.lastname}
-          onChange={handleChange}
-          className="form-control input"
-        />
-      </Col>
+    <Form.Group className="mb-2">
+      <Form.Control
+        required
+        type="text"
+        name="lastname"
+        value={formData.lastname}
+        onChange={handleChange}
+        className="form-control input wide-input"
+        placeholder="Apellido"
+      />
     </Form.Group>
-    <Form.Group as={Row} className="mb-2">
-      <Form.Label column sm={4} className="form-label text-sm-end">Contraseña</Form.Label>
-      <Col sm={8}>
-        <Form.Control
-          required
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          className="form-control input"
-          placeholder="6 o más caracteres"
-        />
-      </Col>
+    <Form.Group className="mb-2">
+      <Form.Control
+        required
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        className="form-control input wide-input"
+        placeholder="Contraseña (6 o más caracteres)"
+      />
     </Form.Group>
-    <Form.Group as={Row} className="mb-2">
-      <Form.Label column sm={4} className="form-label text-sm-end">Confirmar</Form.Label>
-      <Col sm={8}>
-        <Form.Control
-          required
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          className="form-control input"
-        />
-      </Col>
+    <Form.Group className="mb-2">
+      <Form.Control
+        required
+        type="password"
+        name="confirmPassword"
+        value={formData.confirmPassword}
+        onChange={handleChange}
+        className="form-control input wide-input"
+        placeholder="Confirmar contraseña"
+      />
     </Form.Group>
   </>
 );
